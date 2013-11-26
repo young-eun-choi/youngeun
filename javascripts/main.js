a695bedb5790fc6014b8ef2dfd1007031a31aa09
young-eun-choi <choiyeong-eun@choeyeong-euns-MacBook-Pro.local>
2013-11-27 01:10:34 +0900

    hoo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+var scene;
+
+var init = function() {
+    scene = document.querySelector('#scene');    
+};
+
+window.onload = function() {
+    init();  
+};
+
+window.onmousemove = function(e) {
+    var dx = window.innerWidth / 2 - e.pageX;
+    var s = 'rotateY(' + dx + 'deg)'; 
+    scene.style['-webkit-transform'] = s;
+};
